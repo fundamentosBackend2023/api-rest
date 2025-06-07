70eb70d2942d91f002ba6a6e29602605d0e1d9c7
Fundamentos backend <leon@MacBook-Air-de-Leon.local>
2025-06-07 10:55:10 -0600

    add: clients working with mongo

--- routes/clientsRoutes.js
+++ routes/clientsRoutes.js
@@ -4,11 +4,11 @@ const Client = require('../services/clientServices');
 const validationMiddleware = require('./../utils/validationMw');
 const { clientSchema } = require('../utils/schemas');
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     let min = req.query.min;
     let max = req.query.max;
 
-    const filteredClients = Client.getAllClients(min, max);
+    const filteredClients = await Client.getAllClients(min, max);
 
     res.status(200).json({
         message: 'Client list',
@@ -16,56 +16,60 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/vip', (req, res) => {
+router.get('/vip', async (req, res) => {
     const threshold = 1000;
-    const vipClients = Client.getVip(threshold);
+    const vipClients = await Client.getVip(threshold);
     res.status(200).json({
         message: 'VIP Clients',
         vip: vipClients
     });
 });
 
-router.get('/:id', (req, res) => {
-    const clientIndex = req.params.id;
-    const retrievedClient = Client.getOne(clientIndex);
-    res.status(200).json({
-        message: retrievedClient
-    });
+router.get('/:id', async (req, res, next) => {
+    try{
+        const clientIndex = req.params.id;
+        const retrievedClient = await Client.getOne(clientIndex);
+        res.status(200).json({
+            message: retrievedClient
+        });
+    }catch(error){
+        next(error);
+    }
 });
 
 
 router.post('/',
     validationMiddleware(clientSchema),
-    (req, res) => {
+    async (req, res, next) => {
     const receivedClient = req.body;
-    Client.createClient(receivedClient);
+    await Client.createClient(receivedClient);
     res.status(201).json({
         message: 'client successufully registered'
     });
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
     const receivedInfo = req.body;
     const { id: clientIndex } = req.params;
-    Client.updateClient(clientIndex, receivedInfo);
+    await Client.updateClient(clientIndex, receivedInfo);
     res.status(200).json({
         message: 'client updated'
     });
 });
 
 
-router.patch('/addToSpentAmount/:id', (req, res) => {
+router.patch('/addToSpentAmount/:id', async (req, res) => {
     const { id: clientIndex } = req.params;
     const { spentAmount: amountToAdd } = req.body;
-    updatedClient = Client.addSpentAmount(clientIndex, amountToAdd);
+    updatedClient = await Client.addSpentAmount(clientIndex, amountToAdd);
     res.status(200).json({
         newInfo: updatedClient
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
     const { id: clientIndex } = req.params;
-    const clientToDelete = Client.deleteClient(clientIndex);
+    const clientToDelete = await Client.deleteClient(clientIndex);
     res.status(200).json({
         message: 'client delete',
         client: clientToDelete
--- services/clientServices.js
+++ services/clientServices.js
@@ -4,69 +4,55 @@ const boom = require('@hapi/boom');
 class Client{
     constructor(){}
 
-    static getAllClients(min, max){
+    static async getAllClients(min, max){
         if(!max) max = Infinity;
         if(!min) min = 0;
 
-        const filteredClients = {};
-        let filteredCounter = 0;
-        for(const [key, client] of Object.entries(clientsDB)){
-            if(client.spentAmount > min && client.spentAmount < max){
-                filteredClients[filteredCounter] = client;
-                filteredCounter++;
-            }
-        }
+        const filteredClients = await clientsDB.find().where('spentAmount').gte(min).where('spentAmount').lte(max);
 
         return filteredClients;
 
     }
 
-    static createClient(receivedClient){
-        const clientIndex = Object.keys(clientsDB).length;
-        clientsDB[clientIndex] = receivedClient;
-        console.log({clientsDB})
+    static async createClient(receivedClient){
+        const client = new clientsDB(receivedClient);
+        await client.save();
         return true;
     }
 
-    static getVip(threshold){
-        const vipClients = {}
-        let vipCounter = 0
-
-        for(const [key, client] of Object.entries(clientsDB)){
-            if(client.spentAmount >= threshold){
-                vipClients[vipCounter] = client;
-                vipCounter++;
-            }
-        }
+    static async getVip(threshold){
+        const vipClients = await clientsDB.find().where('spentAmount').gt(threshold);
 
         return vipClients;
     }
 
-    static getOne(clientIndex){
-        const retrievedClient = clientsDB[clientIndex];
+    static async getOne(clientIndex){
+        const retrievedClient = await clientsDB.findById(clientIndex);
         if(!retrievedClient){
             throw boom.notFound('Client not found :(')
         }
         return retrievedClient;
     }
 
-    static updateClient(clientIndex, receivedInfo){
-        clientsDB[clientIndex] = receivedInfo;
-        return clientsDB[clientIndex];
+    static async updateClient(clientIndex, receivedInfo){
+        const clientToUpdate = await this.getOne(clientIndex);
+        clientToUpdate.overwrite(receivedInfo);
+        const updatedClient = await clientToUpdate.save();
+        return updatedClient;
     }
 
-    static addSpentAmount(id, amount){
-        clientsDB[id].spentAmount += amount;
-        return clientsDB[id];
+    static async addSpentAmount(id, amount){
+        const client = await this.getOne(id);
+        client.spentAmount += amount;
+        await client.save();
+        return client;
     }
 
-    static deleteClient(clientIndex){
-        const clientToDelete = clientsDB[clientIndex];
-        delete clientsDB[clientIndex];
-        return clientToDelete;
+    static async deleteClient(clientIndex){
+        const client = await this.getOne(clientIndex);
+        await clientsDB.findByIdAndDelete(clientIndex);
+        return client;
     }
-
-
 
 }
 
